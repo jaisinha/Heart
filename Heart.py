99ee9147b0f53a25d21a017dfb514c88df5e5c90
Jai Sinha <72357426+jaisinha@users.noreply.github.com>
2021-04-26 17:30:45 +0530

    first commit

---
+++ Heart.py
@@ -0,0 +1,12 @@
+from turtle import * #import everythin from the package
+color("red","pink")
+begin_fill()
+left(50) #rotate the turtle 50 degrees and then move 100 pixel forward
+circle(40,180) #(radius,angle) the arrow will move 
+left(260)
+circle(40,180)
+forward(100)
+left(104)
+forward(100) # 100pixel
+end_fill()
+done() #to slow down
